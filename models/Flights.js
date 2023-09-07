bd3993311eadb7f70e5d7f5218909653dc3f7cef
rimi <a.fatimaarimi@gmail.com>
2023-09-07 12:04:35 -0400

    confiq

--- models/Flights.js
+++ models/Flights.js
@@ -1,24 +1,28 @@
 const mongoose = require("mongoose");
 
-const flightSchema = new mongoose.Schema(
-  {
-    airline: {
-      type: String, // name
-      enum: ['American', 'Southwest', 'United'],
-      required: true, // required
-    },
-    flightNo: {
-      type: Number,
-      required: true,
-      minLength: 10,
-      maxLength: 9999,
-    },
-    departs: {
-        type: Date,
-    }
+const flightSchema = new mongoose.Schema({
+  airline: {
+    type: String, // name
+    enum: ["American", "Southwest", "United"],
+    required: true, // required
   },
-); 
+  flightNo: {
+    type: Number,
+    required: true,
+    minLength: 10,
+    maxLength: 9999,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now, // Set the default value to the current date
+    required: true,
+  },
+  departs: {
+    type: Date,
+    default: "never",
+  },
+});
 
-// Define a Mongoose model named 'Flight' using the 'mongoose.model' method 
-const Flight = mongoose.model('Flight', flightSchema);
+// Define a Mongoose model named 'Flight' using the 'mongoose.model' method
+const Flight = mongoose.model("Flight", flightSchema);
 module.exports = Flight; // export so we can use the model in our routes
--- server.js
+++ server.js
@@ -1,20 +1,44 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const Flight = require("./models/Flights");
+
+// ** Variables ** //
 const app = express();
 const PORT = 3000;
 
+// ** Middleware  ** //
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // will allow us to receive data as json and send it to our req.body
+
+// ** Routes ** //
 /**
  * Root Route
  * @method GET
  * @description Testing to see is server.js is working
  */
-app.get('/', (req, res) => {
-    console.log("And we are live!");
-    res.send("And we are live!");
+app.get("/", (req, res) => {
+  console.log("And we are live!");
+  res.send("And we are live!");
+});
+
+/**
+ * Show
+ * @method GET
+ * @description Display flights
+ */
+app.get("/flights", async (req, res) => {
+  try {
+    const flights = await Flight.find({});
+    // using the .find method with empty params to grab all flights
+    res.send(flights)
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 /**
  * Live Server
  */
 app.listen(PORT, (req, res) => {
-    console.log(`Server is running on localhost:${PORT}`);
+  console.log(`Server is running on localhost:${PORT}`);
 });
